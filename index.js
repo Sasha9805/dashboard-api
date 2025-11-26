8330f3756752e083084d7fee8df7c57cd0ad6578
Sasha Pimenov <pimenovs33@gmail.com>
2025-11-26 21:22:39 +0200

    Add Router (users example)

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from 'express';
+import { userRouter } from './users/users.js';
 
 const port = 8000;
 
 const app = express();
 
 app.get('/hello', (req, res) => {
-	res.send('Привет из Express!');
+	// res.send('Hello, World!');
+	res.end();
 });
+
+app.use('/users', userRouter);
 
 app.listen(port, () => {
 	console.log(`Express сервер запущен на http://localhost:${port}/`);
