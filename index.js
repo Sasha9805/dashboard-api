ae6e4d0961da14ce75110cb3c63e4fd6b04e0025
Sasha Pimenov <pimenovs33@gmail.com>
2025-11-28 00:38:35 +0200

    Test middleware with use

--- index.js
+++ index.js
@@ -5,12 +5,28 @@ const port = 8000;
 
 const app = express();
 
+app.use((req, res, next) => {
+	console.log('Middleware activated');
+	next();
+});
+
+app.use('/hello', (req, res, next) => {
+	console.log('Hello route middleware activated');
+	next();
+});
+
 app.get('/hello', (req, res) => {
 	// res.send('Hello, World!');
-	res.end();
+	// res.end();
+	throw new Error('Some error occurred');
 });
 
 app.use('/users', userRouter);
+
+app.use((err, req, res, next) => {
+	console.error('Error handling middleware:', err.message);
+	res.status(500).send('Internal Server Error');
+});
 
 app.listen(port, () => {
 	console.log(`Express сервер запущен на http://localhost:${port}/`);
