4daba502c68a40d09ad3f6d72ec70866c353bbc7
Sasha Pimenov <pimenovs33@gmail.com>
2025-12-03 21:24:59 +0200

    Add Typescript

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import { userRouter } from './users/users.js';
 
 const port = 8000;
@@ -23,7 +23,7 @@ app.get('/hello', (req, res) => {
 
 app.use('/users', userRouter);
 
-app.use((err, req, res, next) => {
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 	console.error('Error handling middleware:', err.message);
 	res.status(500).send('Internal Server Error');
 });
